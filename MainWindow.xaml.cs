8da3ee63ba1b2c09a044458c54d54972107f6e20
mikaellacroskrey-HDR <Mikaella.Croskrey@hdrinc.com>
2022-02-14 19:32:40 -0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using ReportHelper.Models;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+
+namespace ReportHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string filePath = string.Empty;
+        public ReportSetting primaryReportSetting;
+        private const string CSV = ".csv";
+        private const string XLSX = ".xlsx";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            primaryReportSetting = new ReportSetting();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+
+            if (!(bool)ofd.ShowDialog())
+            {
+                return;
+            }
+
+            primaryReportSetting.FilePath = ofd.FileName;
+            primaryReportSetting.FileName = ofd.SafeFileName;
+            primaryReportSetting.FolderPath = primaryReportSetting.FilePath.Replace(primaryReportSetting.FileName, "");
+            primaryReportSetting.EquipmentName = primaryReportSetting.FileName.Remove(primaryReportSetting.FileName.LastIndexOf('_'), primaryReportSetting.FileName.Length - primaryReportSetting.FileName.LastIndexOf('_'));
+            txtBoxFilePath.Text = ofd.FileName;
+            List<string> timeStamps = primaryReportSetting.GetTimeStamps();
+            cbStartTime.ItemsSource = timeStamps;
+            cbEndTime.ItemsSource = timeStamps;
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshSettings();
+
+            if (!VerifySettings())
+            {
+                return;
+            }
+
+            Cursor cursor = this.Cursor;
+            this.Cursor = Cursors.Wait;
+               
+            primaryReportSetting.ApplyNameFormatting = (bool)chkBoxNameTemplate.IsChecked;
+
+            primaryReportSetting.DateAltFormat = "XX.XX.XXXX";
+
+            if (DateTime.TryParse(cbStartTime.SelectedItem.ToString(), out DateTime dateTime))
+            {
+                primaryReportSetting.DateAltFormat = $"{dateTime.Month}.{dateTime.Day}.{dateTime.Year}";
+                primaryReportSetting.Date = $"{dateTime.Month}/{dateTime.Day}/{dateTime.Year}";
+            }
+            primaryReportSetting.SavedFilePath = GetNewFileName(primaryReportSetting);
+
+            List<ReportSetting> reportSettings = new List<ReportSetting>();
+
+            if ((bool)chkBoxRunFolder.IsChecked)
+            {
+                // get folder directory
+                string folderPath = primaryReportSetting.FolderPath;
+                
+                // create a list of folders in that folder
+                string[] filesPaths = Directory.GetFiles(folderPath).Where(f => f.Contains(CSV)).ToArray<string>();
+                
+                foreach (string file in filesPaths)
+                {
+                    if(File.Exists(file))
+                    {
+                        ReportSetting reportSetting = new ReportSetting();
+                        reportSetting.FilePath = file;
+                        reportSetting.SavedFilePath = GetNewFileName(reportSetting);
+                        reportSetting.StartTime = primaryReportSetting.StartTime;
+                        reportSetting.EndTime = primaryReportSetting.EndTime;
+                        reportSetting.Date = primaryReportSetting.Date;
+                        reportSetting.FileName = Path.GetFileName(reportSetting.FilePath);
+                        reportSetting.EquipmentName = reportSetting.FileName.Remove(reportSetting.FileName.LastIndexOf('_'), reportSetting.FileName.Length - reportSetting.FileName.LastIndexOf('_'));
+
+                        reportSettings.Add(reportSetting);
+                    }
+                }
+            }
+            else
+            {
+                reportSettings.Add(primaryReportSetting);
+            }
+
+            string successfulFiles = string.Empty;
+            string failedFiles = string.Empty;
+
+            
+            // process all files
+            foreach (ReportSetting reportSetting in reportSettings)
+            {
+                if (ProcessFile(reportSetting))
+                {
+                    successfulFiles += reportSetting.FileName + "\n";
+                }
+                else
+                {
+                    failedFiles += reportSetting.FileName + "\n";
+                }
+            }
+
+            this.Cursor = cursor;
+
+            if (failedFiles.Length > 0)
+            {
+                MessageBox.Show($"Files processed successfully: \n" +
+                    $"{successfulFiles} \n" +
+                    $"Failed files: \n" +
+                    $"{failedFiles}");
+            }
+            else
+            {
+                MessageBox.Show($"Files processed successfully:  \n" +
+                    $"{successfulFiles}");
+            }
+           
+
+        }
+
+        private bool ProcessFile(ReportSetting reportSetting)
+        {
+            if (reportSetting.GenerateReport())
+            {
+                //MessageBox.Show($"{sfd.FileName} has been created!", "Report Helper");
+                return true;
+                
+            }
+            else
+            {
+                //MessageBox.Show("An error occured and the file was not created.", "Report Helper");
+                return false;
+            }
+        }
+
+        private string GetNewFileName(ReportSetting reportSetting)
+        {
+            if (primaryReportSetting.ApplyNameFormatting)
+            {
+                return Path.Combine(primaryReportSetting.FolderPath, $"{ primaryReportSetting.DateAltFormat} {reportSetting.EquipmentName} TMS Raw Data_Not Reviewed");
+            }
+            return reportSetting.FilePath.Replace(".csv", "") + "_Edited" + XLSX;
+
+        }
+
+        /// <summary>
+        /// Read updated values from user inputs.
+        /// </summary>
+        private void RefreshSettings()
+        {
+            primaryReportSetting.ApplyNameFormatting = (bool)chkBoxNameTemplate.IsChecked;
+            primaryReportSetting.StartTime = (string)cbStartTime.SelectedItem;
+            primaryReportSetting.EndTime = (string)cbEndTime.SelectedItem;
+            primaryReportSetting.FilePath = txtBoxFilePath.Text;
+            primaryReportSetting.ForceIntervals = (bool)chkBoxForceIntervals.IsChecked;
+        }
+
+        /// <summary>
+        /// Make sure the user has input valid settings.
+        /// </summary>
+        /// <returns></returns>
+        private bool VerifySettings()
+        {
+            string errors = string.Empty;
+
+            if (!File.Exists(primaryReportSetting.FilePath))
+            {
+                errors += $"File {primaryReportSetting.FilePath} not found.\n";
+            }
+
+            if (string.IsNullOrEmpty(primaryReportSetting.StartTime))
+            {
+                errors += "No start time specified.\n";
+            }
+
+            if (string.IsNullOrEmpty(primaryReportSetting.EndTime))
+            {
+                errors += "No end time specified.\n";
+            }
+
+            if (errors != string.Empty)
+            {
+                MessageBox.Show($"The following error(s) were found with the specified settings.\n" +
+                    $"{errors}" +
+                    $"Please fix and try again.", "Setting Error");
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private void cbStartTime_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshSettings();
+        }
+
+        private void cbEndTime_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshSettings();
+        }
+
+        private void txtBoxFilePath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshSettings();
+        }
+
+        private void chkBoxNameTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshSettings();
+        }
+
+        private void chkBoxForceIntervals_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshSettings();
+        }
+
+        private void chkBoxRunFolder_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshSettings();
+        }
+    }
+}
---
+++ Models/ReportSetting.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Office.Interop.Excel;
+using System.Windows;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ReportHelper.Models
+{
+    public class ReportSetting
+    {
+        public string FileName = string.Empty;
+        public string FilePath = string.Empty;
+        public string FolderPath = string.Empty;
+        public string SavedFilePath = string.Empty;
+        public bool ApplyNameFormatting = false;
+        public bool ForceIntervals = false;
+        public string StartTime = string.Empty;
+        public string EndTime = string.Empty;
+        public string EquipmentName = string.Empty;
+        public string Date = string.Empty;
+        public string DateAltFormat = string.Empty;
+
+        public ReportSetting()
+        {
+        }
+
+        /// <summary>
+        /// Read the file and return a list of all of the Timestamps.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetTimeStamps()
+        {
+            List<string> timeStamps = new List<string>();
+
+            using (TextFieldParser parser = new TextFieldParser(FilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    if (fields.FirstOrDefault() != null && fields.FirstOrDefault().ToUpper() != "TIMESTAMP")
+                    {
+                        timeStamps.Add(fields.First());
+                    }
+                }
+            }
+            return timeStamps;
+        }
+
+        /// <summary>
+        /// Generate an .xlsx file of the transposed data within the specified timespan.
+        /// </summary>
+        public bool GenerateReport()
+        {
+            bool containsStartTime = false;
+            bool containsEndTime = false;
+
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return false;
+            }
+                        
+            object misValue = System.Reflection.Missing.Value;
+            Workbook xlWorkBook = xlApp.Workbooks.Add(misValue);
+            Worksheet xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            bool inInterval = false;
+            int colIdx = 1;
+            int rowIdx = 3;
+
+            try
+            {
+                // Initial report formatting
+                //xlWorkSheet.Name = EquipmentName; // Commenting out because it is not needed and was causing code to fail in some cases.
+                xlWorkSheet.Cells[1, 1] = Date;
+                xlWorkSheet.Cells[2, 1] = EquipmentName;
+
+                Range rg = (Range)xlWorkSheet.Cells[3,1];
+                rg.EntireRow.NumberFormat = "hh:mm:ss AM/PM";
+
+
+                // Read the file and transpose data
+                using (TextFieldParser parser = new TextFieldParser(FilePath))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    DateTime lastDateTime = DateTime.MinValue;
+
+                    while (!parser.EndOfData)
+                    {
+                        string[] fields = parser.ReadFields();
+
+                        // Read line if the first field of the line is a header, or after the start timestamp
+                        if (inInterval || fields.FirstOrDefault() != null && fields.FirstOrDefault().ToUpper() == StartTime.ToUpper() || fields.FirstOrDefault().ToUpper() == "TIMESTAMP")
+                        {
+                            if (fields.FirstOrDefault().ToUpper() != "TIMESTAMP")
+                            {
+                                inInterval = true;
+                                containsStartTime = true;
+                            }
+
+                            foreach (string field in fields)
+                            {
+                                // Insert timestamps to force intervals from 15 min to 5 min if applicable
+                                if (ForceIntervals && rowIdx == 3 && colIdx >= 2)
+                                {
+                                    if (colIdx == 2)
+                                    {
+                                        lastDateTime = DateTime.Parse(field);
+                                    }
+                                    else if (DateTime.Parse(field).Minute - lastDateTime.Minute == 15 || (DateTime.Parse(field).Hour - lastDateTime.Hour == 1 && DateTime.Parse(field).Minute - lastDateTime.Minute == -45))
+                                    {
+                                        xlWorkSheet.Cells[rowIdx, colIdx] = lastDateTime.AddMinutes(5); ;
+                                        xlWorkSheet.Cells[rowIdx, colIdx + 1] = lastDateTime.AddMinutes(10);
+                                        xlWorkSheet.Cells[rowIdx, colIdx + 2] = DateTime.Parse(field);
+                                        lastDateTime = DateTime.Parse(field);
+                                        colIdx += 2;
+                                        rowIdx++;
+                                        continue;
+                                    }
+                                }
+
+                                xlWorkSheet.Cells[rowIdx, colIdx] = field;
+                                rowIdx++;
+                            }
+                            colIdx++;
+                            rowIdx = 3;
+                        }
+
+                        // Stop parsing the file after we get to the end of timespan.
+                        if (fields.FirstOrDefault() != null && fields.FirstOrDefault().ToUpper() == EndTime.ToUpper())
+                        {
+                            containsEndTime = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured generating the .xlsx file. \n " + ex.Message);
+
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+
+                // Free objects
+                Marshal.ReleaseComObject(xlWorkSheet);
+                Marshal.ReleaseComObject(xlWorkBook);
+                Marshal.ReleaseComObject(xlApp);
+                return false;
+            }
+
+            xlWorkSheet.Columns.AutoFit();
+
+            try
+            {
+                //Save and close the Excel file
+                xlWorkBook.SaveAs(SavedFilePath, XlFileFormat.xlOpenXMLWorkbook, misValue,
+                misValue, misValue, misValue, XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            // Free objects
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+            return (containsStartTime && containsEndTime);
+        }
+    }
+}
